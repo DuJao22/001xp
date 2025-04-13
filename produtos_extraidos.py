9d3b165e19525a2d51f917bc3319ad5aba0e7417
João Layon <104402531+DuJao22@users.noreply.github.com>
2025-04-13 17:45:36 -0300

    Add files via upload

---
+++ produtos_extraidos.py
@@ -0,0 +1,124 @@
+produtos = [
+    {
+        "id": 1,
+        "nome": "Ovo de Páscoa do Chef Pistache 600g",
+        "imagem": ["https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwe40064b2/large/1003499_5.png","https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwc1091511/large/1003499_7.png","https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwd686dac3/large/1003499_2.png","https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw33b316db/large/1003499_1t.png"],
+        "preco_antigo": "R$ 199,99",
+        "preco": "R$ 149,99",
+        "etiquetas": [{"texto": "19 CACAUS", "cor": "500"}, {"texto": "Promoção de Páscoa", "cor": "100"}]
+    },
+    {
+        "id": 2,
+        "nome": "Ovo de Páscoa do Chef Nuts Recheado 600g",
+        "imagem": ["https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwf6f99fae/large/1003498_1.png","https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw05d815ca/large/1003498_7.png","https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw8791bef2/large/1003498_2.png","https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw2974a86f/large/1003498_1t.png"],
+        "preco_antigo": "R$ 174,99",
+        "preco": "R$ 119,99",
+        "etiquetas": [{"texto": "8 CACAUS", "cor": "500"}, {"texto": "Promoção de Páscoa", "cor": "100"}]
+    },
+    {
+        "id": 3,
+        "nome": "Ovo de Páscoa ao Leite Bytes Fone Azul 200g",
+        "imagem": ["https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw7ad23b55/large/1003535_1.png",'https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwd8e3dd54/large/1003535_3.png','https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw82c98aa7/large/1003534_2t.png'],
+        "preco_antigo": "R$ 169,99",
+        "preco": "R$ 129,99",
+        "etiquetas": [{"texto": "10 CACAUS", "cor": "500"}, {"texto": "Promoção de Páscoa", "cor": "100"}]
+    },
+    {
+        "id": 4,
+        "nome": "Coelho Cacau Magia ao Leite 850g",
+        "imagem": ['https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwd1c7e45d/large/1003363_1.png','https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwdba022f4/large/1003363_1t.png'],
+        "preco_antigo": "R$ 199,99",
+        "preco": "R$ 169,99",
+        "etiquetas": [{"texto": "13 CACAUS", "cor": "500"}, {"texto": "Promoção de Páscoa", "cor": "100"}]
+    },
+    {
+        "id": 5,
+        "nome": "Ovo de Páscoa ao Leite Pantufa Garfield G 170g",
+        "imagem": ['https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw2446b29f/large/1003259_1.png','https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw78bd659a/large/1003259_3.png','https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw42dcf96a/large/1003519_5.png',"https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw89603c9f/large/1003259_4.png",'https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwf2a15234/large/1003259_5.png','https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwf28ab9ce/large/1003519_2t.png'],
+        "preco_antigo": "R$ 169,99",
+        "preco": "R$ 139,99",
+        "etiquetas": [{"texto": "10 CACAUS", "cor": "500"}, {"texto": "Promoção de Páscoa", "cor": "100"}]
+    },
+    {
+        "id": 6,
+        "nome": "Ovo de Páscoa ao Leite Garrafa Bella's 170g",
+        "imagem": ['https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw3eb3deff/large/1003586_1.png','https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw83eb435b/large/1003586_3.png',"https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw42dcf96a/large/1003519_5.png",'https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwf6e84d71/large/1003586_2.png','https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwf28ab9ce/large/1003519_2t.png'],
+        "preco_antigo": "R$ 149,99",
+        "preco": "R$ 119,99",
+        "etiquetas": [{"texto": "12 CACAUS", "cor": "500"}, {"texto": "Promoção de Páscoa", "cor": "100"}]
+    },
+    {
+        "id": 7,
+        "nome": "Ovo de Páscoa ao Leite Mochila Bob Esponja 170g",
+        "imagem": ['https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwa0f9789e/large/1003575_1.png','https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw26c22988/large/1003575_3.png','https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw836bac9f/large/1003575_4.png','https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw42dcf96a/large/1003519_5.png','https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwf28ab9ce/large/1003519_2t.png'],
+        "preco_antigo": "R$ 139,99",
+        "preco": "R$ 109,99",
+        "etiquetas": [{"texto": "10 CACAUS", "cor": "500"}, {"texto": "Promoção de Páscoa", "cor": "100"}]
+    },
+    {
+        "id": 8,
+        "nome": "Ovo de Páscoa ao Leite Pantufa Ursinhos Carinhosos Ursinha Carinhosa G 170g",
+        "imagem": ["https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw795e26c0/large/1003539_5.png","https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwb25239d5/large/1003539_3.png","https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw42dcf96a/large/1003519_5.png","https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwdc07acb4/large/1003519_1t.png"],
+        "preco_antigo": "R$ 169,99",
+        "preco": "R$ 119,99",
+        "etiquetas": [{"texto": "6 CACAUS", "cor": "500"}, {"texto": "Promoção de Páscoa", "cor": "100"}]
+    },
+    {
+        "id": 9,
+        "nome": "Ovo de Páscoa ao Leite Lata Harry Potter 185g",
+        "imagem": ['https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw249d1218/large/1003615_5.png','https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwfdb19d32/large/1003615_2.png',"https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwfc68cc64/large/1003615_3.png",'https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw89d4bdc2/large/1003615_6.jpeg','https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwa2c580a9/large/1003615_2t.png'],
+        "preco_antigo": "R$ 139,99",
+        "preco": "R$ 119,99",
+        "etiquetas": [{"texto": "12 CACAUS", "cor": "500"}, {"texto": "Promoção de Páscoa", "cor": "100"}]
+    },
+    {
+        "id": 10,
+        "nome": "Ovo de Páscoa ao Leite Chocoesportes Bola NBA 170g",
+        "imagem": ['https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwd50cf8e3/large/1003241_1.png','https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw70a14821/large/1003241_3.png','https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw42dcf96a/large/1003519_5.png','https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw40bd2e37/large/1003241_2.png','https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwf28ab9ce/large/1003519_2t.png'],
+        "preco_antigo": "R$ 124,99",
+        "preco": "R$ 89,99",
+        "etiquetas": [{"texto": "8 CACAUS", "cor": "500"}, {"texto": "Promoção de Páscoa", "cor": "100"}]
+    },
+    {
+        "id": 11,
+        "nome": "Ovo de Páscoa ao Leite Chocoesportes Bola NFL 170g",
+        "imagem": ['https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw66cbec67/large/1003240_5.png','https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw42dcf96a/large/1003519_5.png','https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwb51f1d21/large/1003240_3.png','https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw339ccb16/large/1003240_6.png','https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwf28ab9ce/large/1003519_2t.png'],
+        "preco_antigo": "R$ 94,99",
+        "preco": "R$ 49,99",
+        "etiquetas": [{"texto": "8 CACAUS", "cor": "500"}, {"texto": "Promoção de Páscoa", "cor": "100"}]
+    }
+]
+
+
+produto_promocao = [ 
+    {
+        "id": 1,
+        "titulo": "Ovo de Páscoa do Chef Pistache 600g",
+        "subtitulo": "Edição limitada especial de Páscoa",
+        "info_extra": "Compre e ganhe 10% off na próxima compra",
+        "imagem": [
+            "https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwe40064b2/large/1003499_5.png",
+            "https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwc1091511/large/1003499_7.png",
+            "https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwd686dac3/large/1003499_2.png",
+            "https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw33b316db/large/1003499_1t.png"
+        ],
+        "imagem_secundaria": "https://www.cacaushow.com.br/on/demandware.static/-/Sites-CacauShow-Library/default/dweb1901d8/images/homepage/Desk_339x340_pascoa.jpg",
+        "preco_antigo": "R$ 199,99",
+        "preco": "R$ 149,99",
+        "observacao": "Promoção válida enquanto durarem os estoques.",
+    },
+    {
+        "id": 2,
+        "titulo": "Ovo laCreme 200g",
+        "subtitulo": "Clássico Cacau Show",
+        "info_extra": "Na compra de 2, cada um sai por R$ 49,99",
+        "imagem": [
+            "https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw26676210/medium/1003515_1.png"
+        ],
+        "imagem_secundaria": "https://www.cacaushow.com.br/on/demandware.static/-/Sites-CacauShow-Library/default/dweb1901d8/images/homepage/Desk_339x340_pascoa.jpg",
+        "preco_antigo": "R$ 59,99",
+        "preco": "R$ 49,99",
+        "observacao": "Consulte regulamento no site.",
+    }
+]
+
